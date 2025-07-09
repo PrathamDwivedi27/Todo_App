85d91c91baad8574fccad793919923162f455a5e
Pratham-fursatAI <prathamd549@gmail.com>
2025-07-09 20:30:31 +0530

    basic tab structure is done

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,35 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{}}
+    >
+        <Tabs.Screen
+            name='index'
+            options={{
+                title: "Todos",
+                tabBarIcon: ({color,size})=>(
+                    <Ionicons name='flash-outline' size={size}/>
+                )
+            }}
+        />
+
+        <Tabs.Screen
+            name='settings'
+            options={{
+                title: "Settings",
+                tabBarIcon: ({color,size})=>(
+                    <Ionicons name='settings' size={size}/>
+                )
+            }}
+        />
+        
+    </Tabs>
+  )
+}
+
+export default TabsLayout
