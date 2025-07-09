febb18620cb163086d389c7463aebbfdc904a8e5
Pratham-fursatAI <prathamd549@gmail.com>
2025-07-10 00:38:10 +0530

    loading spinner

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,10 +5,20 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "@/components/Header";
 import TodoInput from "@/components/TodoInput";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Index() {
   const { toggleDarkMode, colors } = useTheme();
+
   const homeStyles = createHomeStyles(colors);
+
+  const todos = useQuery(api.todos.getTodos);
+
+  const isLoading= todos === undefined;
+
+  if(isLoading) return <LoadingSpinner />;
 
   return (
     <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
